3c174bdce58f09282bfbd34fd383df37a4d35c5f
Henry Alumona <henryalumona@meta.com>
2023-06-26 12:29:57 -0700

    Designed shopping cart functionality

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./ShoppingCart.css";
 import ProductCard from "../ProductCard/ProductCard";
 
-export default function ShoppingCart() {
+export default function ShoppingCart({products}) {
   const [cartItems, setCartItems] = useState({}); //Initializing the state of the shopping cart to empty (zero), umtil theres an increment
 
   const handleIncrement = (productId) => {
@@ -19,18 +19,99 @@ export default function ShoppingCart() {
     }));
   }; //5. Defined the handleIncrement, handleDecrement functions to be passed in the ProductCard
 
-  return (
+
+  const getProductById = (productId) => {
+    return products.find((product) => product.id === productId);
+  };
+
+  const calculateSubtotal = () => {
+    let subtotal = 0;
+    Object.keys(cartItems).forEach((productId) => {
+      const product = getProductById(productId);
+      const cost = product.price * cartItems[productId];
+      subtotal += cost;
+    });
+    return subtotal;
+  };
+
+  const calculateTaxes = () => {
+    const subtotal = calculateSubtotal();
+    return 0.09 * subtotal;
+  };
+
+  const calculateTotal = () => {
+    const subtotal = calculateSubtotal();
+    const taxes = calculateTaxes();
+    return subtotal + taxes;
+  };
+
+//   return (
+//     <div className="shopping-cart">
+//       <h2>Shopping Cart</h2>
+//       {Object.keys(cartItems).map((productId) => (
+//         <ProductCard
+//           key={productId}
+//           product={product}
+//           quantity={cartItems[productId]}
+//           handleIncrement={handleIncrement}
+//           handleDecrement={handleDecrement}
+//         />
+//       ))}
+//     </div>
+//   );
+
+
+return (
     <div className="shopping-cart">
-      <h2>Shopping Cart</h2>
-      {Object.keys(cartItems).map((productId) => (
-        <ProductCard
-          key={productId}
-          product={product}
-          quantity={cartItems[productId]}
-          handleIncrement={handleIncrement}
-          handleDecrement={handleDecrement}
-        />
-      ))}
+      <h2><span role="img" aria-label="Cart">🛒</span>{" "} Shopping Cart</h2>              {/*  6. To Display the title with cart emoji */}
+      {Object.keys(cartItems).length === 0 ? (                                           // 7.  Check if cartItems is empty
+        <p>You have no items in your shopping cart</p>                                   // 8. if empty, Display a message for an empty cart
+      ) : (                                                                              //else create a table with the folloeing information
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Quantity</th>
+                <th>Unit Price</th>
+                <th>Cost</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Generate rows for each item in the cart */}
+              {Object.keys(cartItems).map((productId) => {
+                const item = cartItems[productId];
+                const product = getProductById(productId); // Replaced with my own function to get product by ID (where it eas fetched by product id)
+                const cost = product.price * item;
+                return (
+                  <tr key={productId}>
+                    <td>{product.name}</td>
+                    <td>{item}</td>
+                    <td>${product.price.toFixed(2)}</td>
+                    <td>${cost.toFixed(2)}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <table>
+            <tbody>
+              <tr>
+                <td>Subtotal</td>
+                <td>${calculateSubtotal().toFixed(2)}</td> 
+              </tr>
+              <tr>
+                <td>Taxes and fees</td>
+                <td>${calculateTaxes().toFixed(2)}</td> 
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>${calculateTotal().toFixed(2)}</td> 
+              </tr>
+            </tbody>
+          </table>
+        </>
+      )}
     </div>
   );
 } 
--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -29,30 +29,30 @@ import { MdAddShoppingCart } from "react-icons/md"
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai"
 import { BsCashCoin, BsCreditCard } from "react-icons/bs"
 import "./Sidebar.css"
-// import ShoppingCart from "../ShoppingCart/ShoppingCart"
-// import CheckoutForm from "../CheckoutForm/CheckoutForm"
+import ShoppingCart from "../ShoppingCart/ShoppingCart"
 
-export default function Sidebar() {
+export default function Sidebar({ products }) {
 	const [isOpen, setIsOpen] = useState(false)
 
 	const handleToggle = () => {
 		setIsOpen(!isOpen)
-	}
+  };
 
-	return (
-		<aside className={`sidebar ${isOpen ? "open" : ""}`}>
-			<div className="toggle-btn" onClick={handleToggle}>
-				{isOpen ? <AiOutlineArrowLeft className="arrow" /> : <AiOutlineArrowRight className="arrow" />}
-			</div>
-			<div className={`sidebar-icons ${isOpen ? "open" : ""}`}>
-				<MdAddShoppingCart className="sidebar-icon" onClick={handleToggle}/>
-				<BsCashCoin className="sidebar-icon" onClick={handleToggle}/>
-				<BsCreditCard className="sidebar-icon" onClick={handleToggle}/>
-			</div>
-			<div className="sidebar-content">
-				{/* <ShoppingCart />
-				<CheckoutForm /> */}
-			</div>
-		</aside>
-	)
+  return (
+    <aside className={`sidebar ${isOpen ? "open" : ""}`}>
+      <div className="toggle-btn" onClick={handleToggle}>
+        {isOpen ? (
+          <AiOutlineArrowLeft className="arrow" />
+        ) : (
+          <AiOutlineArrowRight className="arrow" />
+        )}
+      </div>
+      <div className={`sidebar-icons ${isOpen ? "open" : ""}`}>
+        <MdAddShoppingCart className="sidebar-icon" onClick={handleToggle} />
+        <BsCashCoin className="sidebar-icon" onClick={handleToggle} />
+        <BsCreditCard className="sidebar-icon" onClick={handleToggle} />
+      </div>
+      {isOpen && <ShoppingCart products={products} />} {/* Renders the ShoppingCart component when the cart is open */}
+    </aside>
+  );
 }
