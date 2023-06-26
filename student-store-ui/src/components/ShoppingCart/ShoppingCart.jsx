696ac00a29c5b37e3e7d83748bf8af586a8e4922
Henry Alumona <henryalumona@meta.com>
2023-06-25 18:44:09 -0700

    Defined the +ment, -ment functions, passed in product card

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./ShoppingCart.css";
+import ProductCard from "../ProductCard/ProductCard";
 
 export default function ShoppingCart() {
   const [cartItems, setCartItems] = useState({}); //Initializing the state of the shopping cart to empty (zero), umtil theres an increment
@@ -16,5 +17,20 @@ export default function ShoppingCart() {
       ...prevItems,
       [productId]: Math.max((prevItems[productId] || 0) - 1, 0)
     }));
-  };
-}
+  }; //5. Defined the handleIncrement, handleDecrement functions to be passed in the ProductCard
+
+  return (
+    <div className="shopping-cart">
+      <h2>Shopping Cart</h2>
+      {Object.keys(cartItems).map((productId) => (
+        <ProductCard
+          key={productId}
+          product={product}
+          quantity={cartItems[productId]}
+          handleIncrement={handleIncrement}
+          handleDecrement={handleDecrement}
+        />
+      ))}
+    </div>
+  );
+} 
