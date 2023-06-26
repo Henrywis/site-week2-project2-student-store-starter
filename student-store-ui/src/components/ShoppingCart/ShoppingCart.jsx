df417d623503180d735e9f6d2cf93eda625d7bca
Henry Alumona <henryalumona@meta.com>
2023-06-25 17:49:02 -0700

    Initializing Cart state before increments

---
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import "./ShoppingCart.css";
+
+export default function ShoppingCart() {
+  const [cartItems, setCartItems] = useState({}); //Initializing the state of the shopping cart to empty (zero), umtil theres an increment
+
+  
+}
