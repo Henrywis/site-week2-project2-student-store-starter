dc76b6a0bf146c74426b9dc5b5c432fdae617d55
Henry Alumona <henryalumona@meta.com>
2023-06-25 18:37:29 -0700

    Defined the handlers for increment/decrement in ShoppingCart

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -4,5 +4,17 @@ import "./ShoppingCart.css";
 export default function ShoppingCart() {
   const [cartItems, setCartItems] = useState({}); //Initializing the state of the shopping cart to empty (zero), umtil theres an increment
 
-  
+  const handleIncrement = (productId) => {
+    setCartItems((prevItems) => ({
+      ...prevItems,
+      [productId]: (prevItems[productId] || 0) + 1
+    }));
+  };
+
+  const handleDecrement = (productId) => {
+    setCartItems((prevItems) => ({
+      ...prevItems,
+      [productId]: Math.max((prevItems[productId] || 0) - 1, 0)
+    }));
+  };
 }
