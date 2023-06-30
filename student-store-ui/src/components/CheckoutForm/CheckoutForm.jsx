a82177d498639ef6216eed3248cad70a8de74c72
Henry Alumona <henryalumona@meta.com>
2023-06-29 19:59:30 -0700

    Initialized the state of checkOutForm prop

--- student-store-ui/src/components/CheckoutForm/CheckoutForm.jsx
+++ student-store-ui/src/components/CheckoutForm/CheckoutForm.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./CheckoutForm.css";
+import { useState } from "react";
 import { BsCashCoin, BsCreditCard } from "react-icons/bs"
 
 export default function CheckoutForm ({ isOpen, shoppingCart, checkOutForm, handleOnCheckoutFormChange, handleOnSubmitCheckoutForm }) {
+    const [checkOutForm, setCheckOuTForm] = useState({name: "", email: ""});
+    
     return (
         <form onSubmit={handleOnSubmitCheckoutForm}>
           <label>
